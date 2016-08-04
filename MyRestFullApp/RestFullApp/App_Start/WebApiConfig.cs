8cbb2bc13a38e2b5806ab25caa2eda588e61a962
romercepeda <romercepeda@gmail.com>
2016-08-04 03:38:19 -0300

    Project de VitulMind
    Test

---
+++ MyRestFullApp/RestFullApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace RestFullApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "MyRestFullApp/{controller}/{action}/{id}",
+                //Se modifico el ruteo de los API para cumplir con el requerimiento de http://localhost:8080/MyResftfullApp/Cotizacion/MONEDA,
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MyRestFullApp/SqlQuery_1.sql
@@ -0,0 +1,44 @@
+﻿
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [Popular];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[User]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[User];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'User'
+CREATE TABLE [dbo].[User] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(50)  NOT NULL,
+    [LastName] nvarchar(50)  NOT NULL,
+    [Email] nvarchar(50)  NOT NULL,
+    [Password] nvarchar(15)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'User'
+ALTER TABLE [dbo].[User]
+ADD CONSTRAINT [PK_User]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
